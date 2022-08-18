e88d4b9b2aa161221163631c0ef862c534e5e53f
ugo-rodriguez <ugo.rdgz@gmail.com>
2022-08-18 21:44:46 +0200

    add argparse part

---
+++ main.py
@@ -0,0 +1,15 @@
+###Libraries
+import argparse
+
+
+
+###main
+parser = argparse.ArgumentParser()
+parser.add_argument("name_input",help="it's the name of the input image")
+parser.add_argument("name_output",help="it's the name of the output image")
+args = parser.parse_args()
+
+
+
+
+
