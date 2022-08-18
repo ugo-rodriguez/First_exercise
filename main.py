cb5a3261e0a188b3095fbcbacb95681eba36c56e
ugo-rodriguez <ugo.rdgz@gmail.com>
2022-08-18 22:09:33 +0200

    add images and itk lines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ###Libraries
 import argparse
+import itk
 
 
 
@@ -9,7 +10,11 @@ parser.add_argument("name_input",help="it's the name of the input image")
 parser.add_argument("name_output",help="it's the name of the output image")
 args = parser.parse_args()
 
+input_filename = args.name_input
+output_filename = args.name_output
 
+pixel_type = itk.ctype("unsigned char")
+image = itk.imread(input_filename,pixel_type)
 
-
-
+median = itk.median_image_filter(image, radius=10)
+itk.imwrite(median, output_filename)
